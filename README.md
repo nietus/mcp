c4912053676d8fcff14fea707e35863fbe38d4c4
Niet <antonionetodev@yahoo.com>
2025-05-09 17:40:01 -0300

    calculator

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mcp
+
 Criação de MCPs simples para preparação para um projeto real de maior complexidade
---
+++ calculator/main.py
@@ -0,0 +1,23 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(
+    name="calculator",
+    host="0.0.0.0",
+    port=8000,
+)
+
+@mcp.tool()
+def add(a: int, b: int):
+    return a + b
+
+@mcp.tool()
+def subtract(a: int, b: int):
+    return a - b
+
+@mcp.tool()
+def multiply(a: int, b: int):
+    return a * b
+
+
+if __name__ == "__main__":
+    mcp.run()
